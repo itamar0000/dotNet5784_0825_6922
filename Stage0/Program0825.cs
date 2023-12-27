9223a404a4c355ba64a2cea3f2231580115a1099
Itamar Haimov <ihaimov@acad.jct.ac.il>
2023-12-27 16:26:15 +0200

    stage0 - welcome0825

---
+++ Stage0/Program0825.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome6922();
+        static void Main(string[] args)
+        {
+            Welcome0825();
+            Welcome6922();
+
+            Console.ReadKey();
+
+        }
+
+          private static void Welcome0825()
+        {
+            Console.WriteLine("Enter your name:");
+            string? username = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", username);
+        }
+    }
+}
