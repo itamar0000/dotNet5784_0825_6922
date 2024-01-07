fdc4bca97f7bcbce5a1bb1580f3e458289dff561
Itamar Haimov <ihaimov@acad.jct.ac.il>
2024-01-08 00:58:18 +0200

    meeged secsusfully

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -15,56 +15,127 @@ internal class Program
     {
         try
         {
-           Initialization.Do(s_dalEngineer,s_dalDependencys, s_dalTask);
-            int a =int.Parse(Console.ReadLine()!);
-            Console.WriteLine("choose:\n"+"0.Exit\n"+"1.Open Engineer menu\n"+"2.Open Task menu\n"+"3.Open Dependency menu\n");
-            switch (a)
+            Initialization.Do(s_dalEngineer, s_dalDependencys, s_dalTask);
+            int a = 0;
+            do
             {
-                case 1:
-                    {
-                        
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.WriteLine("choose:\n" +
-                            "1.Exit\n"
-                            "2.Create Task\n" + 
-                            "3.Read Task\n" + 
-                            "4.ReadAll Task\n" +
-                            "5.Update Task\n" +
-                            "6.Delete Task\n" +
-                            "7.");
-                        int b = int.Parse(Console.ReadLine()!);
-                        switch (b)
+                Console.WriteLine("choose:\n" + "0.Exit\n" + "1.Open Engineer menu\n" + "2.Open Task menu\n" + "3.Open Dependency menu\n");
+                a = int.Parse(Console.ReadLine()!);
+                switch (a)
+                {
+                    case 1:
                         {
-                            case 1:
-                                {
-                                    Console.WriteLine("Enter Task's alias:");
-                                    string? alias = Console.ReadLine();
-                                    Console.WriteLine("Enter Task's description:");
-                                    string? description = Console.ReadLine();
-                                    Console.WriteLine("Enter Task's start date:");
-                                    DateTime start = DateTime.Parse(Console.ReadLine()!);
-                                    Console.WriteLine("Enter Task's end date:");
-                                    DateTime end = DateTime.Parse(Console.ReadLine()!);
-                                    break;
-                                }
+                            MenuEngineer();
+                            break;
                         }
-
-
-                        break;
-                    }
+                    case 2:
+                        {
+                            MenuTask();
+                            break;
+                        }
+                    case 3:
+                        {
+                            MenuDependency();
+                            break;
+                        }
+                }
             }
- 
-
+            while (a != 0);
         }
         catch (Exception)
         {
 
         }
     }
-    private void MenuEngineer()
+
+    private static void MenuTask()
+    {
+        Console.WriteLine("choose:\n" +
+            "1.Exit\n" +
+            "2.Create Task\n" +
+            "3.Read Task\n" +
+            "4.ReadAll Task\n" +
+            "5.Update Task\n" +
+            "6.Delete Task");
+        int b = int.Parse(Console.ReadLine()!);
+        switch (b)
+        {
+            case 1:
+                {
+                    break;
+                }
+            case 2:
+                {
+                    Task item = InputTask();
+                    s_dalTask!.Create(item);
+                    break;
+                }
+            case 3:
+                {
+                    Console.WriteLine("Enter Task's id:\n");
+                    int id = int.Parse(Console.ReadLine()!);
+                    Task? item = s_dalTask!.Read(id);
+                    if (item != null)
+                        Console.WriteLine(item);
+                    else Console.WriteLine("Task with this ID does not exist");
+                    break;
+                }
+            case 4:
+                {
+                    List<Task> newTasks = new();
+                    newTasks = s_dalTask!.ReadAll();
+                    foreach (Task item in newTasks)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                }
+            case 5:
+                {
+                    int id = int.Parse(Console.ReadLine()!);
+                    Task item = InputTask();
+                    s_dalTask!.Update(item);
+                    break;
+                }
+            case 6:
+                {
+                    Console.WriteLine("Enter ID of task to delete: ");
+                    int id = int.Parse(Console.ReadLine()!);
+                    s_dalTask!.Delete(id);
+                    break;
+                }
+        }
+    }
+
+    private static Task InputTask()
+    {
+        Console.WriteLine("Enter Task's alias:\n");
+        string? alias = Console.ReadLine();
+        Console.WriteLine("Enter Task's description:\n");
+        string? description = Console.ReadLine();
+        Console.WriteLine("Enter Task's complexity:\n");
+        DO.EngineerExperience? Complexity = (DO.EngineerExperience)(int.Parse(Console.ReadLine()));
+        Console.WriteLine("Enter Task's scheduled date:\n");
+        DateTime scheduledDate = DateTime.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter Task's start date:\n");
+        DateTime start = DateTime.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter Task's required effort time:\n");
+        TimeSpan required = TimeSpan.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter Task's deadline date:\n");
+        DateTime deadline = DateTime.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter Task's deliverables:\n");
+        string? deliverables = Console.ReadLine();
+        Console.WriteLine("Enter Task's remarks:\n");
+        string? remarks = Console.ReadLine();
+        Console.WriteLine("Enter Task's engineer Id:\n");
+        int? engineerId = int.Parse(Console.ReadLine());
+        Task item = new(0, alias, description, DateTime.Now, false, Complexity,
+                        scheduledDate, start, required, deadline, null, deliverables,
+                        remarks, engineerId);
+        return item;
+    }
+
+    private static void MenuEngineer()
     {
         Console.WriteLine("choose:\n" + "1.Create Engineer\n" + "2.Read Engineer\n" + "3.ReadAll Engineer\n" + "4.Update Engineer\n" + "5.Delete Engineer\n" + "6.Exit");
         int b = int.Parse(Console.ReadLine()!);
@@ -104,6 +175,10 @@ internal class Program
                 {
                     List<DO.Engineer> newEngineers = new();
                     newEngineers = s_dalEngineer!.ReadAll();
+                    foreach (Engineer item in newEngineers)
+                    {
+                        Console.WriteLine(item);
+                    }
                     break;
                 }
             case 4:
@@ -126,5 +201,66 @@ internal class Program
                 break;
         }
     }
-}
+    private static void MenuDependency()
+    {
+        Console.WriteLine("0.Exit\n" + "choose:\n" + "1.Create Dependency\n" + "2.Read Dependency\n" + "3.ReadAll Dependency\n" + "4.Update Dpenedency\n" + "5.Delete Dependency");
+        int b = int.Parse(Console.ReadLine()!);
+        switch (b)
+        {
+            case 1:
+                {
+                    Dependency dependency = getInputDependency();
+                    s_dalDependencys.Create(dependency);
+                    break;
+                }
+            case 2:
+                {
+                    Console.WriteLine("Enter Dependency ID");
+                    Dependency? check = s_dalDependencys!.Read(int.Parse(Console.ReadLine()!));
+                    if (check != null)
+                    {
+                        Console.WriteLine(check);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dependency with this ID does not exist");
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    List<DO.Dependency> newDependencies = new();
+                    newDependencies = s_dalDependencys!.ReadAll();
+                    foreach (Dependency dependency in newDependencies)
+                    {
+                        Console.WriteLine(dependency);
+                    }
+                    break;
+                }
+            case 4:
+                {
 
+                    Console.WriteLine("Enter Dependency ID");
+                    int id = int.Parse(Console.ReadLine()!);
+                    Dependency a = s_dalDependencys.Read(id);
+                    Console.WriteLine(a);
+                    Dependency x = getInputDependency();
+                    s_dalDependencys.Update(x);
+                    break;
+                }
+            case 0:
+                break;
+        }
+
+    }
+
+    private static Dependency getInputDependency()
+    {
+        Console.WriteLine("Enter task that depends ID");
+        int? id = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the tas it depends on ID");
+        int? id2 = int.Parse(Console.ReadLine());
+        Dependency dependency = new(0, id, id2);
+        return dependency;
+    }
+}
