44743e59b2f246bc197e7a2d15c5e1ebd7fe12cd
Benaya Trabelsi <btrabels@g.jct.ac.il>
2024-02-06 19:04:59 +0200

    Program functions

--- BL/BlApi/ITask.cs
+++ BL/BlApi/ITask.cs
@@ -9,6 +9,6 @@ public interface ITask
     public void Update(BO.Task item);
     public void Update(int id, DateTime date);
     public BO.Task? Read(int id);
-    public BO.Task? Read(Func<BO.Task?, bool>? filter = null)
+    public BO.Task? Read(Func<BO.Task?, bool>? filter = null);
     public IEnumerable<BO.Task> ReadAll(Func<BO.Task?, bool>? filter = null);
 }
