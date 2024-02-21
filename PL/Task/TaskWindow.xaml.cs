b17a6901f9ef1098833f0c0870315208c18ce202
Benaya Trabelsi <btrabels@g.jct.ac.il>
2024-02-21 13:55:40 +0200

    Task Window

--- PL/Task/TaskWindow.xaml.cs
+++ PL/Task/TaskWindow.xaml.cs
@@ -17,11 +17,71 @@ namespace PL.Task
     /// <summary>
     /// Interaction logic for TaskWindow.xaml
     /// </summary>
+    /// 
+
     public partial class TaskWindow : Window
     {
-        public TaskWindow(int id=0)
+
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+
+        public BO.Task task
+        {
+            get { return (BO.Task)GetValue(TaskProperty); }
+            set { SetValue(TaskProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        int id;
+        public static readonly DependencyProperty TaskProperty =
+            DependencyProperty.Register("task", typeof(BO.Task), typeof(TaskWindow), new PropertyMetadata(null));
+
+
+        public TaskWindow(int Id = 0)
         {
             InitializeComponent();
+            id = Id;
+            if (Id == 0)
+            {
+                task = new BO.Task();
+            }
+            else
+            {
+                try
+                {
+                    task = s_bl.Task.Read(Id)!;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+
+        private void Add_UpdateTask_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    s_bl.Task.Create(task);
+                    MessageBox.Show("Task added successfully", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+                else
+                {
+                    s_bl.Task.Update(task);
+                    MessageBox.Show("Task updated successfully", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
+
